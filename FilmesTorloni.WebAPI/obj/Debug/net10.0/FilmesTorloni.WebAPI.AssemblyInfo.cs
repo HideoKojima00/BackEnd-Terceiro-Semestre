72b3ed12bff9a89dff525f8a895991207a3e59bc
FELIPE SILVA DOS SANTOS FONTES <46290704869@info.local>
2026-03-19 10:06:51 -0300

    foi adicionado as ultimas atividades

--- FilmesTorloni.WebAPI/obj/Debug/net10.0/FilmesTorloni.WebAPI.AssemblyInfo.cs
+++ FilmesTorloni.WebAPI/obj/Debug/net10.0/FilmesTorloni.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesTorloni.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96ddd068c330ccb514d7aafcfbc57597bed88985")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesTorloni.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesTorloni.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
